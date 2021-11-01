32f0bb0e6c2d19bc12d52f0c8d1650445c40777e
mintropy <jun2korea@gmail.com>
2021-11-01 18:18:45 +0900

    1101

--- 이영준/1101/15954.py
+++ 이영준/1101/15954.py
@@ -3,23 +3,20 @@ Title : 인형들
 Link : https://www.acmicpc.net/problem/15954
 """
 
-import decimal
 import sys
 input = sys.stdin.readline
 MIIS = lambda: map(int, input().split())
 
 
-def calc_variation(nums, avg):
-    variation = decimal.Decimal(0)
+def calc_variation(nums: list, avg: int) -> int:
+    variation = 0
     for num in nums:
         variation += (num - avg) ** 2
-    return variation / decimal.Decimal(len(nums))
+    return variation / len(dolls_now)
 
 
 N, K = MIIS()
 dolls = list(MIIS())
-for i in range(N):
-    dolls[i] = decimal.Decimal(dolls[i])
 
 standard_deviations = []
 
@@ -27,8 +24,8 @@ for i in range(N - K + 1):
     # K개 이상 연속 확인
     for j in range(i + K, N + 1):
         dolls_now = dolls[i:j]
-        avg = decimal.Decimal(str(sum(dolls_now) / K))
+        avg = sum(dolls_now) / len(dolls_now)
         variation = calc_variation(dolls_now, avg)
-        standard_deviations.append(variation.sqrt())
+        standard_deviations.append(variation ** 0.5)
 
 print(min(standard_deviations))
